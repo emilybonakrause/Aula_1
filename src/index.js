5b6fdad5d4d961a4469c06da06c3fe0d714926eb
emilybonakrause <125470104+emilybonakrause@users.noreply.github.com>
2023-02-15 10:46:36 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+//declarando variável com let
+let numero= 1
+{
+
+  
+  let número = 2
+  console.log (' dentro =',numero) 
+  
+  }
+console.log(' fora =',numero) 
+
+//declarando variável com var
+var numero = 1
+
+
+{
+  
+  var numero1 = 2
+  console.log('dentro1 =',numero1)
+  
+  }
+
+console.log ('fora1 =', numero1)
+
+//declarando uma constante 
+
+const numero2 = 10
+numero2 = 11
